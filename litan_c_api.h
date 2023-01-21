bb2d74c1eacaf8031d83c25851f6e3e4711b6603
Jene Litsch <litsch.jene@gmail.com>
2023-01-21 01:22:27 +0100

    Added CApi struct

---
+++ litan_c_api.h
@@ -0,0 +1,17 @@
+#ifndef LTN_C_API_H
+#define LTN_C_API_H
+#include <stdint.h>
+
+struct CApi {
+	void * core;
+
+	int64_t (*param_int)(uint64_t index, void * core);
+	uint8_t (*param_bool)(uint64_t index, void * core);
+	double (*param_float)(uint64_t index, void * core);
+
+	void (*return_int)(int64_t value,  void * core);
+	void (*return_bool)(uint8_t value, void * core);
+	void (*return_float)(double value, void * core);
+};
+
+#endif
