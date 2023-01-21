d1d075fbf9fc1467325c62449529857b30c9d6b3
Jene Litsch <litsch.jene@gmail.com>
2023-01-21 22:03:48 +0100

    Support for library side objects

--- litan_c_api.h
+++ litan_c_api.h
@@ -14,11 +14,13 @@ struct ltn_CApi {
 	uint8_t (*param_bool)(uint64_t index, void * core);
 	double (*param_float)(uint64_t index, void * core);
 	const char * (*param_string)(uint64_t index, void * core);
+	struct ltn_CObject (*param_object) (uint64_t index, void * core);
 
-	void (*return_int)(int64_t value,  void * core);
-	void (*return_bool)(uint8_t value, void * core);
-	void (*return_float)(double value, void * core);
-	void (*return_string)(const char * value, void * core);
+	void (*return_int) (int64_t value,  void * core);
+	void (*return_bool) (uint8_t value, void * core);
+	void (*return_float) (double value, void * core);
+	void (*return_string) (const char * value, void * core);
+	void (*return_object) (struct ltn_CObject value, void * core);
 };
 
 #endif
