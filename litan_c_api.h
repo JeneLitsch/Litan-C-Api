6617d480867c69d4d89ded25ff5f449751598829
Jene Litsch <litsch.jene@gmail.com>
2023-01-21 22:03:48 +0100

    Added extern c linkage to structs

--- litan_c_api.h
+++ litan_c_api.h
@@ -2,23 +2,28 @@
 #define LTN_C_API_H
 #include <stdint.h>
 
-struct ltn_CObject {
-	void * ptr;
-	void(*deleter)(void *);
-};
+extern "C" {
+	struct ltn_CObject {
+		void * ptr;
+		void(*deleter)(void *);
+	};
 
-struct ltn_CApi {
-	void * core;
+	struct ltn_CApi {
+		void * core;
 
-	int64_t (*param_int)(uint64_t index, void * core);
-	uint8_t (*param_bool)(uint64_t index, void * core);
-	double (*param_float)(uint64_t index, void * core);
-	const char * (*param_string)(uint64_t index, void * core);
+		int64_t (*param_int)(uint64_t index, void * core);
+		uint8_t (*param_bool)(uint64_t index, void * core);
+		double (*param_float)(uint64_t index, void * core);
+		const char * (*param_string)(uint64_t index, void * core);
+		struct ltn_CObject (*param_object) (uint64_t index, void * core);
 
-	void (*return_int)(int64_t value,  void * core);
-	void (*return_bool)(uint8_t value, void * core);
-	void (*return_float)(double value, void * core);
-	void (*return_string)(const char * value, void * core);
-};
+		void (*return_int) (int64_t value,  void * core);
+		void (*return_bool) (uint8_t value, void * core);
+		void (*return_float) (double value, void * core);
+		void (*return_string) (const char * value, void * core);
+		void (*return_object) (struct ltn_CObject value, void * core);
+	};
+}
+
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,5 +1,6 @@
 #include "litan_c_api.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void powf_2(struct ltn_CApi * api) {
 	double x = api->param_float(0, api->core);
@@ -26,4 +27,31 @@ void hello_world(struct ltn_CApi * api) {
 void print(struct ltn_CApi * api) {
 	const char * s = api->param_string(0, api->core);
 	printf(s);
+}
+
+
+struct S {
+	int a;
+	int b;
+};
+
+void free_s(void * ptr) {
+	free(ptr);
+}
+
+void make(struct ltn_CApi * api) {
+	struct ltn_CObject obj;
+	struct S * s = (struct S*) malloc(sizeof(struct S));
+	s->a = 42;
+	s->b = 1337;
+	obj.ptr = (void *)s;
+	obj.deleter = free_s;
+	api->return_object(obj, api->core);
+}
+
+
+void use(struct ltn_CApi * api) {
+	struct ltn_CObject obj = api->param_object(0, api->core);
+	printf("a: %d\n", ((struct S*)obj.ptr)->a);
+	printf("b: %d\n", ((struct S*)obj.ptr)->b);
 }
